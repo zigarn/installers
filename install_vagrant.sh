4b83db5a74856e79b01212ba66cbce33bd3d05ec
Alexandre Garnier <alexandre.garnier@zenika.com>
2019-11-03 17:03:41 +0100

    Install single binary for vagrant

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -2,8 +2,8 @@
 
 VERSION=$(curl --silent --location https://api.github.com/repos/hashicorp/vagrant/tags | jq --raw-output '.[0].name | sub("^v"; "")')
 echo "Installing vagrant ${VERSION}"
-PACKAGE=$(mktemp)
-curl --silent --location -o ${PACKAGE} https://releases.hashicorp.com/vagrant/${VERSION}/vagrant_${VERSION}_$(uname --machine).deb \
-	&& dpkg --install ${PACKAGE}
-rm --force ${PACKAGE}
+ARCHIVE=$(mktemp)
+curl --silent --location -o ${ARCHIVE} https://releases.hashicorp.com/vagrant/${VERSION}/vagrant_${VERSION}_$(uname --kernel-name | tr '[:upper:]' '[:lower:]')_$(dpkg --print-architecture).zip \
+	&& unzip -oqq ${ARCHIVE} -d /usr/local/bin/
+rm --force ${ARCHIVE}
 curl --silent --location https://raw.githubusercontent.com/hashicorp/vagrant/master/contrib/bash/completion.sh > /etc/bash_completion.d/vagrant
