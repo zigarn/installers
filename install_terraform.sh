1d42a442175a245a94f4567e784f2527172cf5fe
Alexandre Garnier <zigarn@gmail.com>
2020-09-01 08:58:55 +0200

    Use terraform provided autocompletion

--- install_terraform.sh
+++ install_terraform.sh
@@ -11,4 +11,4 @@ ARCHIVE=$(mktemp)
 curl --silent --location -o ${ARCHIVE} https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_$(uname --kernel-name | tr '[:upper:]' '[:lower:]')_$(dpkg --print-architecture).zip \
 	&& unzip -oqq ${ARCHIVE} -d /usr/local/bin/
 rm --force ${ARCHIVE}
-curl --silent --location https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/terraform.completion.bash > /etc/bash_completion.d/terraform
+echo 'complete -C /usr/local/bin/terraform terraform' > /etc/bash_completion.d/terraform
