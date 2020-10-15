c7ec30adb53e29d570e188ce004784ea9d93eae0
Alexandre Garnier <alexandre.garnier@zenika.com>
2020-10-15 15:39:22 +0200

    Update kubectx/kubens to go version
    And enable them as kubectl plugins

--- install_kubectx.sh
+++ install_kubectx.sh
@@ -2,9 +2,9 @@
 
 VERSION=$(curl --silent --location https://api.github.com/repos/ahmetb/kubectx/releases/latest | jq --raw-output .tag_name)
 echo "Installing kubectx ${VERSION}"
-curl --silent --location https://raw.githubusercontent.com/ahmetb/kubectx/${VERSION}/kubectx > /usr/local/bin/kubectx \
-	&& chmod +x /usr/local/bin/kubectx
-curl --silent --location https://raw.githubusercontent.com/ahmetb/kubectx/${VERSION}/kubens > /usr/local/bin/kubens \
-	&& chmod +x /usr/local/bin/kubens
+curl --silent --location https://github.com/ahmetb/kubectx/releases/download/${VERSION}/kubectx_${VERSION}_linux_x86_64.tar.gz | tar xzf - --directory /usr/local/bin/ kubectx
+ln -sf kubectx /usr/local/bin/kubectl-ctx
+curl --silent --location https://github.com/ahmetb/kubectx/releases/download/${VERSION}/kubens_${VERSION}_linux_x86_64.tar.gz | tar xzf - --directory  /usr/local/bin/ kubens
+ln -sf kubens /usr/local/bin/kubectl-ns
 curl --silent --location https://raw.githubusercontent.com/ahmetb/kubectx/${VERSION}/completion/kubectx.bash > /etc/bash_completion.d/kubectx
 curl --silent --location https://raw.githubusercontent.com/ahmetb/kubectx/${VERSION}/completion/kubens.bash > /etc/bash_completion.d/kubens
