52e8f9fe070d5b3b7064052c175ab935d4172c82
Alexandre Garnier <zigarn@gmail.com>
2021-10-05 22:10:35 +0200

    Adapt docker-compose installer for compose v2

--- install_docker-compose.sh
+++ install_docker-compose.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-CURRENT=$(docker-compose version --short 2>/dev/null)
+CURRENT=$(docker compose version --short 2>/dev/null)
 VERSION=$(curl --silent --location https://api.github.com/repos/docker/compose/releases/latest | jq --raw-output .tag_name)
 if [ "${CURRENT}" = "${VERSION}" ]; then
 	echo "Already latest docker-compose ${CURRENT}"
 	exit
 fi
 echo "Installing docker-compose ${VERSION} over ${CURRENT:-None}"
-curl --silent --location https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname --kernel-name)-$(uname --machine) > /usr/local/bin/docker-compose \
-	&& chmod +x /usr/local/bin/docker-compose
-curl --silent --location https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+mkdir --parent /usr/local/lib/docker/cli-plugins
+curl --silent --location https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname --kernel-name)-$(uname --machine) > /usr/local/lib/docker/cli-plugins/docker-compose \
+	&& chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+#curl --silent --location https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
